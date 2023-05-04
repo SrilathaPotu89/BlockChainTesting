55c5bbb94370466efb4ce6e55e70e4c871cb5819
SrilathaPotu89 <srilatha.p@systemsdna.com>
2023-05-04 10:04:28 +0530

    Contractgit1update

--- ContractGit1.sol
+++ ContractGit1.sol
@@ -5,7 +5,8 @@ import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 
 contract LiquidityValueCalculator is ILiquidityValueCalculator {
-    function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint totalSupply) {
+  // Conract1
+  function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint totalSupply) {
         IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
         totalSupply = pair.totalSupply();
         (uint reserves0, uint reserves1,) = pair.getReserves();
